e58b9539482b5139a805f449d8c5058c36eae274
Javier Moreno <javmorenov@gmail.com>
2018-04-18 13:37:54 +0200

    New project Graddle

--- README.md
+++ README.md
@@ -4,24 +4,25 @@ Tweet Spell Checker (TweetSC) is a spell checker for tweets. It corrects errors 
 ## How to compile / Requirements
 
 - Java 1.8
-- maven? gradle?
+- Gradle
 
 ```
 git clone https://github.com/jmorenov/TweetSC
-mvn clean install 
+mvn clean install
 ```
 
 ## How to run an example
 
-java -jar XXX 
+java -jar XXX
 
 ## Where to find test files
 
 TweetNorm...
 
+## Final Master's Work Report
 
-## Memoria de Trabajo Fin de Grado
+https://github.com/jmorenov/TweetSC/blob/master/documentation/Report.pdf
 
-enlace a
+## Software versioning
 
-We use Semantic Versioning for the versions of the project. https://semver.org 
+We use Semantic Versioning for the versions of the project. https://semver.org
--- code/TweetSpellChecker/download_dependencies.bat
+++ code/TweetSpellChecker/download_dependencies.bat
@@ -1,6 +1,7 @@
-curl -LO http://nlp.stanford.edu/software/stanford-corenlp-full-2018-02-27.zip
-unzip stanford-corenlp-full-2018-02-27.zip
-mv stanford-corenlp-full-2018-02-27/stanford-corenlp-3.9.1-models.jar lib/stanford-corenlp-3.9.1-models.jar
-rm -R stanford-corenlp-full-2018-02-27
-rm stanford-corenlp-full-2018-02-27.zip
-curl -o lib/stanford-spanish-corenlp-2018-02-27-models.jar https://nlp.stanford.edu/software/stanford-spanish-corenlp-2018-02-27-models.jar
+curl http://nlp.stanford.edu/software/stanford-corenlp-full-2018-02-27.zip
+jar xf stanford-corenlp-full-2018-02-27.zip
+move stanford-corenlp-full-2018-02-27/stanford-corenlp-3.9.1-models.jar lib/stanford-corenlp-3.9.1-models.jar
+del stanford-corenlp-full-2018-02-27
+del stanford-corenlp-full-2018-02-27.zip
+curl https://nlp.stanford.edu/software/stanford-spanish-corenlp-2018-02-27-models.jar
+move stanford-spanish-corenlp-2018-02-27-models.jar lib/stanford-spanish-corenlp-2018-02-27-models.jar
