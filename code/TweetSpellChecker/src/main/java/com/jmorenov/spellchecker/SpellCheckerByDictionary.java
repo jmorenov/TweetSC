7c59b79814006880b81e4d5f7fa934dfbc3b0179
Javier Moreno <javmorenov@gmail.com>
2018-04-20 00:24:25 +0200

    Working with spell checker by dictionary

--- code/TweetSpellChecker/src/main/java/com/jmorenov/spellchecker/SpellCheckerByDictionary.java
+++ code/TweetSpellChecker/src/main/java/com/jmorenov/spellchecker/SpellCheckerByDictionary.java
@@ -3,22 +3,42 @@ package com.jmorenov.spellchecker;
 import com.jmorenov.postagging.StanfordNLP;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpellCheckerByDictionary {
-    //private _dictionaryWords
-    //private _ownNames
-    //private alphabet
+    private Map<String, Integer> _dictionaryWords;
+    private Map<String, Integer> _ownNames;
+    private String alphabet;
 
     SpellCheckerByDictionary(String text) {
-
+        //_dictionaryWords = train(getWords())
     }
 
-    /*private ArrayList<String> words() {
-        return
-    }*/
+    private ArrayList<String> getWords(String text) {
+        ArrayList<String> words = new ArrayList<>();
+        Pattern p = Pattern.compile("[\\w']+");
+        Matcher m = p.matcher(text);
 
-    private void train(ArrayList<String> features) {
+        while (m.find()) {
+            words.add(text.substring(m.start(), m.end()));
+        }
 
+        return words;
+    }
+
+    private Map<String, Integer> train(ArrayList<String> words) {
+        Map<String, Integer> dictionary = new HashMap<String, Integer>();
+
+        for (String word : words) {
+            int wordPcValue = dictionary.get(word) + 1;
+
+            dictionary.put(word, wordPcValue);
+        }
+
+        return dictionary;
     }
 
     private void edits1(String word) {
