0611afba042515d59d59aae6bf4f1640c5f0eb90
Javier Moreno <jmorenov28@gmail.com>
2018-07-01 11:53:07 +0200

    Add Freeling

--- code/tweetsccore/src/main/java/com/jmorenov/tweetsccore/extra/FreelingInitializator.java
+++ code/tweetsccore/src/main/java/com/jmorenov/tweetsccore/extra/FreelingInitializator.java
@@ -14,13 +14,15 @@ public class FreelingInitializator {
      * Constructor of the class
      */
     public static String init() {
-        String freeling_javaAPIPath = FreelingInitializator.class.getClassLoader().getResource("freeling_javaAPI.so").getPath();
-        String libfreelingPath = FreelingInitializator.class.getClassLoader().getResource("freeling_javaAPI.so").getPath();
-
-        System.load(freeling_javaAPIPath);
-        System.load(libfreelingPath);
-
         String freelingDataPath =  Paths.get("src","main","resources", "freeling").toAbsolutePath() + "/";
+        String libfreelingPath = FreelingInitializator.class.getClassLoader().getResource("libfreeling.so").getPath();
+        String freeling_javaAPIPath = FreelingInitializator.class.getClassLoader().getResource("libfreeling_javaAPI.so").getPath();
+        //String libfreelingPath = freelingDataPath + "libfreeling.jnilib";
+        //String freeling_javaAPIPath = freelingDataPath + "libfreeling_javaAPI.jnilib";
+
+        System.load(libfreelingPath);
+        System.load(freeling_javaAPIPath);
+
         Util.initLocale( "default" );
 
         return freelingDataPath;
