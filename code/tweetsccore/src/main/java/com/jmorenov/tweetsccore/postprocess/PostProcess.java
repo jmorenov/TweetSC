498262286bb99e3c696c1f6b812c459d1afdb39d
Javier Moreno <jmorenov28@gmail.com>
2018-07-09 20:41:26 +0200

    Add Dictionaries singleton

--- code/tweetsccore/src/main/java/com/jmorenov/tweetsccore/postprocess/PostProcess.java
+++ code/tweetsccore/src/main/java/com/jmorenov/tweetsccore/postprocess/PostProcess.java
@@ -1,6 +1,16 @@
 package com.jmorenov.tweetsccore.postprocess;
 
+/**
+ * PostProcess class to apply postprocess
+ *
+ * @author <a href="mailto:jmorenov28@gmail.com">Javier Moreno</a>
+ */
 public class PostProcess {
+    /**
+     * Method to apply postprocess
+     * @param text String
+     * @return String
+     */
     public static String apply(String text) {
         return text;
     }
